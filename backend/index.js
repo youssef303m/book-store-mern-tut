faba0d427a50a98ad75fce6f692659c7765d1372
Youssef303M <youssef303m@gmail.com>
2024-01-07 07:39:47 +0200

    Added mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,21 @@
 import express from "express";
+import mongoose from "mongoose";
 import "dotenv/config";
 
 const app = express();
+const mongoURL = process.env.MONGO_URL;
 const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
-  console.log(`App listening on port: ${PORT}`);
-});
+app.get("/", (req, res) => {});
+
+mongoose
+  .connect(mongoURL)
+  .then(() => {
+    console.log("MongoDB Server Running.");
+    app.listen(PORT, () => {
+      console.log(`App listening on port: ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
