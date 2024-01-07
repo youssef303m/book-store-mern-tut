1751aa9a2d0f392abac7204b8689a06e4f90410b
Youssef303M <youssef303m@gmail.com>
2024-01-07 08:43:07 +0200

    fix(cors): configured cors policy

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,22 @@ import express from "express";
 import mongoose from "mongoose";
 import "dotenv/config";
 import booksRouter from "./routes/booksRoute.js";
+import cors from "cors";
 
 const app = express();
 const mongoURL = process.env.MONGO_URL;
 const PORT = process.env.PORT;
+
+// CORS Middleware
+// Allow all origins
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
+
 // Middleware for parsing request body
 app.use(express.json());
 
